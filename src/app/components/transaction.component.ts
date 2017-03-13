00a8939f8b9dbe01827df2536e379aab90366a3d
deepak <deepak@MCGL-5406.COMVIVA.COM>
2017-03-13 08:56:12 +0530

    Varaibles Added

--- src/app/components/transaction.component.ts
+++ src/app/components/transaction.component.ts
@@ -19,10 +19,9 @@ export class TransactionComponent  {
   showPgTransaction :boolean =false;
   showPgIntegration :boolean =false;
   showAgTransaction:boolean =false;
-  showOfTrannsacton:boolean=false;
+  showOfTransacton:boolean=false;
   httpService:PostsService;
   submitted: boolean = false;
-  showMessage = true;
   message:string;
   customResponse:CustomResponse;
   constructor(private postsService:PostsService,private formBuilder: FormBuilder){
@@ -30,17 +29,19 @@ export class TransactionComponent  {
   }
   ngOnInit() {
     this.name = 'Transaction Information';
-    this.model = new CustomRequest('1170312125242322');
+    this.model = new CustomRequest('1170309002956256');
     this.transactionForm = this.formBuilder.group({
       orderNumbers:     [this.model.orderNumbers, Validators.required]
     });
-    this.message="No Data Found";
   }
 
   onSubmit({ value, valid }: { value: CustomRequest, valid: boolean }) {
     console.log(JSON.stringify(value));
     this.postsService.postRequest('FETCH_ORDER_INFO',value).subscribe((posts:CustomResponse)=>{
       this.showPgTransaction = false;
+      this.showPgIntegration  = false;
+      this.showAgTransaction = false;
+      this.showOfTransacton = false;
       this.customResponse = posts
       console.log("Status =>"+JSON.stringify(this.customResponse.status));
       console.log("createdOn =>"+this.customResponse.ofTransaction.created_on);
@@ -48,11 +49,10 @@ export class TransactionComponent  {
       if(posts.status=='A500'){
         this.message  ="Failed To Refund amount. Please check application logs"
       }
-      this.showMessage = true;
       this.showPgTransaction = true;
-      this.showPgIntegration  = false;
-      this.showAgTransaction = false;
-      this.showOfTrannsacton = false;
+      this.showPgIntegration  = true;
+      this.showAgTransaction = true;
+      this.showOfTransacton = true;
     });
   }
 }
